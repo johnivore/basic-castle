6c42ae1fb8fc4d65b3a1456dd89a7a393de9870d
John Begenisich <john.begenisich@outlook.com>
2021-04-15 18:28:42 +0800

    add XFS to 'dfd' function

--- home/.config/fish/functions/dfd.fish
+++ home/.config/fish/functions/dfd.fish
@@ -1,4 +1,4 @@
 # docker and snaps really fuck up df
 function dfd
-    df -h --type ext4 --type ext3 --type ext2 --type vfat --type iso9660 --type btrfs
+    df -h --type xfs --type ext4 --type ext3 --type ext2 --type vfat --type iso9660 --type btrfs
 end
